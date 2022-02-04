ae146b145c3605d10878775490e81859c726be7f
Michael Ochmann <michael.ochmann@propeller.de>
2022-02-04 13:45:32 +0100

    added combined literal and printf test

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,11 @@ const {log, level} = require("../index");
 
 
 const LogTest = () => {
+	const foo = 10;
+
 	log("this is a normal string");
 	log("in this string, this '%' gets replaced with the number five and this not '\\%'", 5);
-	log(level.INFO`this is just informational`);
+	log(level.INFO`this is just informational because foo (${foo}) is %`, foo);
 	log(level.WARN`in this literal, this '%' gets replaced with the number five and this not '\\%'`, 5);
 	log(level.SUCCESS`success!`);
 	log(level.ERROR`ERROR -> this is an error`);
