db8ae317a6c65a71920d20ffcd6c0d1020b82eda
Michael Ochmann <michael.ochmann@propeller.de>
2022-11-22 00:43:19 +0100

    fix(Scheduler) improved performance, added documentation
    * now using the `Number()` constructor instead of `parseInt()` because
      of better performance
    * added doxygen comments for better intellisense output and usage
      information

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const {Time}       = require("./Time");
-const {log, level} = require("./Log");
+const {Time}   = require("./Time");
+const {assert} = require("./Log");
 
 class TimeCallback {
 	seconds;
@@ -41,32 +41,32 @@ class Scheduler {
 		const seconds    = now.getSeconds();
 		const minutes    = now.getMinutes();
 		const hours      = now.getHours();
-		const dayOfMonth = now.getDate();
+		const dayOfMonth = now.geDate();
 		const month      = now.getMonth();
 		const dayOfWeek  = now.getDay();;
 
 		for (const callback of this.callbacks) {
-			if (typeof callback.seconds === "string" && seconds % parseInt(callback.seconds) !== 0)
+			if (typeof callback.seconds === "string" && seconds % Number(callback.seconds) !== 0)
 				continue;
 			else if (typeof callback.seconds === "number" && seconds !== callback.seconds)
 				continue;
-			if (typeof callback.minutes === "string" && minutes % parseInt(callback.minutes) !== 0)
+			if (typeof callback.minutes === "string" && minutes % Number(callback.minutes) !== 0)
 				continue;
 			else if (typeof callback.minutes === "number" && minutes !== callback.minutes)
 				continue;
-			if (typeof callback.hours === "string" && hours % parseInt(callback.hours) !== 0)
+			if (typeof callback.hours === "string" && hours % Number(callback.hours) !== 0)
 				continue;
 			else if (typeof callback.hours === "number" && hours !== callback.hours)
 				continue;
-			if (typeof callback.dayOfMonth === "string" && dayOfMonth % parseInt(callback.dayOfMonth) !== 0)
+			if (typeof callback.dayOfMonth === "string" && dayOfMonth % Number(callback.dayOfMonth) !== 0)
 				continue;
 			else if (typeof callback.dayOfMonth === "number" && dayOfMonth !== callback.dayOfMonth)
 				continue;
-			if (typeof callback.month === "string" && month % parseInt(callback.month) !== 0)
+			if (typeof callback.month === "string" && month % Number(callback.month) !== 0)
 				continue;
 			else if (typeof callback.month === "number" && month !== callback.month)
 				continue;
-			if (typeof callback.dayOfWeek === "string" && dayOfWeek % parseInt(callback.dayOfWeek) !== 0)
+			if (typeof callback.dayOfWeek === "string" && dayOfWeek % Number(callback.dayOfWeek) !== 0)
 				continue;
 			else if (typeof callback.dayOfWeek === "number" && dayOfWeek !== callback.dayOfWeek)
 				continue;
@@ -75,30 +75,48 @@ class Scheduler {
 		}
 	}
 
+	/**
+	 * Register a scheduled callback
+	 * 
+	 * The `timeString` argument takes a syntax very similar to the linux crontab:
+	 * 
+	 * - a number means the literal number - i.e. `5` on the "minutes" place means "on the fifth minute"
+	 * - a `*` means every unit - i.e. `*` on the "hours" place means "every hour"
+	 * * `* /n` means every "nth" unit - i.e. `* /6` on the "seconds" place means "every six seconds"
+	 * 
+	 * ``` 
+	 * *    *    *    *    *    *
+	 * ┬    ┬    ┬    ┬    ┬    ┬
+	 * │    │    │    │    │    │
+	 * │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+	 * │    │    │    │    └───── month (1 - 12)
+	 * │    │    │    └────────── day of month (1 - 31)
+	 * │    │    └─────────────── hours (0 - 23)
+	 * │    └──────────────────── minutes (0 - 59)
+	 * └───────────────────────── seconds (0 - 59)
+	 * ```
+	 *  
+	 * @param {string}   timeString 
+	 * @param {function} callback 
+	 */
 	register(timeString, callback) {
+		assert(typeof callback === "function", "the 'callback' parameter has to be a function/callable");
 		this.callbacks.push(new TimeCallback(...Scheduler.ParseTimeString(timeString), callback));
 	}
 
 	static ParseTimeString = string => {
 		const parts = string.split(/\s/gi);
 		
-		if (parts.length !== 6) {
-			log(level.ERROR`time string '%' is malformed`, string);
-
-			return [];
-		}
+		assert(parts.length === 6, "time string '%' is malformed", string);
 
 		for (let i = 0; i < parts.length; i++) {
-			const int = parseInt(parts[i]);
+			const int = Number(parts[i]);
 			if (isNaN(parts[i])) {
 				if (parts[i] === '*') {
 					parts[i] = null;
 					continue;
 				}
-				if (!parts[i].match(/\*\/?[1-9]*/)) {
-					log(level.ERROR`unknown time indentifier: '%'`, parts[i]);
-					return [];
-				}
+				assert(parts[i].match(/\*\/?[1-9]*/), "unknown time indentifier: '%'", parts[i]);
 				parts[i] = `${parts[i].split('/')[1]}`;
 
 				// '*/1' is equal to '*'
@@ -106,10 +124,8 @@ class Scheduler {
 					parts[i] = null;
 				continue;
 			}
-
 			parts[i] = int;
 		}
-		log("parts: %", parts);
 
 		return parts;
 	}
