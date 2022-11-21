a0c9449b212f5df95d83e56f568559a85699a4b9
Michael Ochmann <michael.ochmann@propeller.de>
2022-11-21 16:03:07 +0100

    fix(Scheduler): resolved circular include

--- index.js
+++ index.js
@@ -7,9 +7,10 @@
 const {log, level, logDirect, assert} = require("./src/Log");
 const {ucFirst, sprintf, printf}      = require("./src/Strings");
 const {isDevelopment, here}           = require("./src/Environment");
-const {Time, Scheduler}               = require("./src/Time");
+const {Time}                          = require("./src/Time");
 const {rand}                          = require("./src/Math");
 const {Enum, iota}                    = require("./src/Enum");
+const {Scheduler}                     = require("./src/Scheduler");
 
 module.exports = {
 	log,
